515099a7ee56bc9532e115bc7e60bc3fb1f2ee80
msf <msf@matthewfinifter.com>
2021-08-21 14:06:12 -0700

    a bit of cleanup

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
     "bufio"
     "flag"
     "fmt"
-    "log"
+    "io"
     "os"
     "regexp"
     "sort"
@@ -59,26 +59,51 @@ func main() {
     flag.Parse()
 
     var allLines []string
+    var err error
 
     if inputFileName == "" {
-        allLines = readAllLinesFromStdin()
+        allLines, err = readAllLinesFromStdin()
+        if err != nil {
+            fmt.Printf("Error reading from stdin: %v", err)
+            return
+        }
     } else {
-        allLines = readAllLinesFromFile(inputFileName)
+        allLines, err = readAllLinesFromFile(inputFileName)
+        if err != nil {
+            fmt.Printf("Error reading from file '%v': %v", inputFileName, err)
+            return
+        }
     }
 
-    currentMatchDay := 1
-    matchDayPlayers := map[string]bool{}
-    cumulativeScores := map[string]int{}
+    resultString, err := calculateResults(allLines)
+    if err != nil {
+        fmt.Printf("Encountered an error in result calculation: %v", err)
+    } else {
+        fmt.Print(resultString)
+    }
+}
 
-    for _, line := range allLines {
-        team1, score1, team2, score2 := parseLine(line)
+func calculateResults(inputLines []string) (string, error) {
+    ret := "" // The string we will eventually return
+
+    currentMatchDay := 1 // State: current match day being processed
+    matchDayPlayers := map[string]bool{} // State: for the current match day, keep track of which players we've seen already
+    cumulativeScores := map[string]int{} // State: cumulative scores so far, across match days
+
+    for _, line := range inputLines {
+        team1, score1, team2, score2, err := parseLine(line)
+        if err != nil {
+            return "", err
+        }
 
         if contains(matchDayPlayers, team1) || contains(matchDayPlayers, team2) {
-            outputMatchDayResults(currentMatchDay, cumulativeScores)
-            matchDayPlayers = map[string]bool{}
+            // Repeat player indicates end of previous match day.
+            ret += matchDayResults(currentMatchDay, cumulativeScores) + "\n" // Append match day results to output
+            matchDayPlayers = map[string]bool{} // Reset players we've seen so far for current match day
             currentMatchDay++
         }
 
+        // Mark team1 and team2 as having been seen for current match day
         matchDayPlayers[team1] = true
         matchDayPlayers[team2] = true
 
@@ -87,14 +112,15 @@ func main() {
             addToScore(cumulativeScores, team2, 1)
         } else if score1 > score2 {
             addToScore(cumulativeScores, team1, 3)
-            addToScore(cumulativeScores, team2, 0)
+            addToScore(cumulativeScores, team2, 0) // Add zero to ensure the team is present.
         } else {
-            addToScore(cumulativeScores, team1, 0)
+            addToScore(cumulativeScores, team1, 0) // Add zero to ensure the team is present.
             addToScore(cumulativeScores, team2, 3)
         }
     }
 
-    outputMatchDayResults(currentMatchDay, cumulativeScores)
+    ret += matchDayResults(currentMatchDay, cumulativeScores) // Nothing left to process. Append final match day results to output.
+    return ret, nil
 }
 
 func addToScore(scores map[string]int, team string, scoreToAdd int) {
@@ -110,16 +136,17 @@ func contains(theMap map[string]bool, team string) bool {
     return ok
 }
 
-func outputMatchDayResults(currentMatchDay int, matchDayScores map[string]int) {
-    fmt.Printf("Matchday %v\n", currentMatchDay)
+func matchDayResults(currentMatchDay int, matchDayScores map[string]int) string {
+    ret := ""
+    ret += fmt.Sprintf("Matchday %v\n", currentMatchDay)
 
     sortable := NewSortable(matchDayScores)
     sort.Sort(sortable)
 
     for i := 0; i < len(sortable.teams) && i < 3; i++ {
-        fmt.Printf("%v, %v pt%v\n", sortable.teams[i], sortable.scores[i], pluralize(sortable.scores[i]))
+        ret += fmt.Sprintf("%v, %v pt%v\n", sortable.teams[i], sortable.scores[i], pluralize(sortable.scores[i]))
     }
-    fmt.Println()
+    return ret
 }
 
 func pluralize(score int) string {
@@ -129,51 +156,47 @@ func pluralize(score int) string {
     return "s"
 }
 
-func parseLine(line string) (string, int, string, int) {
+func parseLine(line string) (string, int, string, int, error) {
     matches := lineRegexp.FindStringSubmatch(line)
     if matches == nil || len(matches) != 5 {
-        log.Fatal("regexp did not match line")
+        return "", 0, "", 0, fmt.Errorf("regexp did not match line: %v", line)
     }
     score1, err1 := strconv.Atoi(matches[2])
+    if err1 != nil {
+        return "", 0, "", 0, fmt.Errorf("unable to convert score to int: %v", matches[2])
+    }
     score2, err2 := strconv.Atoi(matches[4])
-    if err1 != nil || err2 != nil {
-        log.Fatal("unable to convert score to int")
+    if err2 != nil {
+        return "", 0, "", 0, fmt.Errorf("unable to convert score to int: %v", matches[4])
     }
 
-    return matches[1], score1, matches[3], score2
+    return matches[1], score1, matches[3], score2, nil
 }
 
-func readAllLinesFromStdin() []string {
+func readAllLinesFromReader(r io.Reader) ([]string, error) {
     var ret []string
-    scanner := bufio.NewScanner(os.Stdin)
+    scanner := bufio.NewScanner(r)
     for scanner.Scan() {
         ret = append(ret, scanner.Text())
     }
 
     if err := scanner.Err(); err != nil {
-        log.Fatal(err)
+        return nil, err
     }
 
-    return ret
+    return ret, nil
 }
 
-func readAllLinesFromFile(filename string) []string {
-    var ret []string
+func readAllLinesFromStdin() ([]string, error) {
+    return readAllLinesFromReader(os.Stdin)
+}
+
+func readAllLinesFromFile(filename string) ([]string, error) {
     file, err := os.Open(filename)
     if err != nil {
-        log.Fatal(err)
+        return nil, err
     }
     defer file.Close()
 
-    // TODO DRY
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        ret = append(ret, scanner.Text())
-    }
-
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-
-    return ret
+    return readAllLinesFromReader(file)
 }
